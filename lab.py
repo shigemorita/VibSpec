d16256cac0ba7d8566dcd081142d97574ea2bdb7
Shigeaki Morita <39329577+shigemorita@users.noreply.github.com>
2022-02-01 21:30:18 +0900

    Delete lab.py

--- lab.py
+++
@@ -1,8 +0,0 @@
-import pandas
-
-def read_csv(filename):
-	data = pandas.read_csv(filename, header=0, index_col=0).T
-	data.index = pandas.read_csv(filename, header=None, index_col=0).iloc[0].values
-	return data
-
-if __name__ == '__main__': print("lab.py")
