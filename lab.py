ca59334a67b745a32264b8582b99151479f8eefb
Shigeaki Morita <39329577+shigemorita@users.noreply.github.com>
2022-02-01 09:21:06 +0900

    Add files via upload

--- lab.py
+++ lab.py
@@ -2,8 +2,7 @@ import pandas
 
 def read_csv(filename):
 	data = pandas.read_csv(filename, header=0, index_col=0).T
-	data.index = pandas.read_csv(filename, header=None, index_col=0).iloc[0]
-	data.index.name=""
+	data.index = pandas.read_csv(filename, header=None, index_col=0).iloc[0].values
 	return data
 
-if __name__ == '__main__': print("")
+if __name__ == '__main__': print("lab.py")
